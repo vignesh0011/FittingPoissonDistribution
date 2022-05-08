8ecf2d17f871a1793f145087ca9359396ceb0b14
Vignesh Mamidi <53014593+vignesh0011@users.noreply.github.com>
2022-05-08 09:55:36 +0530

    Create poisson.py

---
+++ poisson.py
@@ -0,0 +1,25 @@
+import numpy as np
+import math
+import scipy.stats
+X=[0,1,2,3,4,5,6]
+f=[153,169,72,31,12,6,2]
+n=6
+N=np.sum(f)
+mean=np.inner(X,f)/N
+Prob=list(); E=list(); xi=list()
+print("  X P(X=x) Obs.Fr  Ex.Fre   xi ")
+print("----------------------------------")
+for x in range(7):
+    Prob.append(math.exp(-mean)*mean*x/math.factorial(x))
+    E.append(Prob[x]*N)
+    xi.append((f[x]-E[x])**2/E[x])
+    print("%2.2f %2.2f  %4.2f   %3.2f   %3.2f"%(x,Prob[x],f[x],E[x],xi[x]))
+print("----------------------------------")
+cal_chi2=np.sum(xi)
+print("Calculated value of Chi square is %4.2f"%cal_chi2)
+tab_chi2=scipy.stats.chi2.ppf(1-.01, df=n)
+print("Table value of Chi square at 1  level is %4.2f"%tab_chi2)
+if cal_chi2<tab_chi2:
+    print("The given data can be fitted in Poissson distribution at 1% LOS")
+else:
+    print("The given data cannot be fitted in Poisson distribution at 1% LOS")
